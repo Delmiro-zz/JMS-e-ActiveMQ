c17f00e2b5927b6de6f7731031dd6ee81d6cdb81
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-02-19 23:18:37 -0300

    Consumindo mensagens com JMS.

---
+++ src/br/com/caelum/jms/TesteConsumidor.java
@@ -0,0 +1,37 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteConsumidor {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext context = new InitialContext();
+
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+		
+		Session session  = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination fila  = (Destination) context.lookup("xpto");
+		MessageConsumer consumer = session.createConsumer(fila);
+		
+		Message message = consumer.receive();
+		
+		System.out.println("Recebendo a mensagem:" + message);
+		
+		connection.close();
+		context.close();
+	}
+}
